e92a443c7a1fb0ae8334457a2777788ebfe76cef
joonasmykkanen <mykkanen.joonas@outlook.com>
2025-09-08 12:36:28 +0200

    fix: remove unsuded comments

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,7 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const trackEvent = (action: string, params?: { [key: string]: any }) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (typeof window.gtag !== "undefined") {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     window.gtag('event', action, params);
   }
 };
