c9392235e66e5446e1a4308baa32da2df8ed2e65
joonasmykkanen <mykkanen.joonas@outlook.com>
2025-11-07 20:13:06 +0200

    feat: add two houses to sale

--- data/Apartments.tsx
+++ data/Apartments.tsx
@@ -16,23 +16,21 @@ export type Apartment = {
 };
 
 const rawApartments: Apartment[] = [
-  // {
-  //   id: 'A32',
-  //   name: 'Täysin remontoitu kolmio',
-  //   type: '3h+k+p',
-  //   price: 51000,
-  //   rent: 930,
-  //   fee: 332.5,
-  //   roi: 14.08,
-  //   coc: 36.01,
-  //   sale: true,
-  //   desc: `Juuri muokattu soluksi nostamalla uusi väliseinä keittiön ja olohuoneen väliin. Poikkeuksellisen kovat vuokrasopimukset sekä juuri kesällä 2025 remontoitu keittiö.
-  //     \n
-  //     Lähettämällä viestin tai lomakkeen 'A32' saat asunnosta vielä 1000€ lisä alennuksen jos kaupat toteutuvat ennen 1.11.2025!🤑
-  //   `,
-  //   images: [],
-  //   status: 'vapaa',
-  // },
+  {
+    id: 'A32',
+    name: 'Täysin remontoitu kolmio',
+    type: '3h+k+p',
+    price: 52990,
+    rent: 927,
+    fee: 332.5,
+    roi: 13.46,
+    coc: 37.88,
+    sale: false,
+    desc: `Juuri muokattu soluksi nostamalla uusi väliseinä keittiön ja olohuoneen väliin. Poikkeuksellisen kovat vuokrasopimukset sekä juuri kesällä 2025 remontoitu keittiö.
+    `,
+    images: [],
+    status: 'vapaa',
+  },
   {
     id: 'A31',
     name: 'Täysin remontoitu kaksio',
@@ -68,27 +66,27 @@ const rawApartments: Apartment[] = [
     price: 52990,
     rent: 863.00,
     fee: 322.5,
-    roi: 12.24,
-    coc: 33.80,
+    roi: 11.69,
+    coc: 31.98,
     sale: false,
     desc: "Aikoinaan remontoitu solu kolmio. Pinnat eivät ole enään uudet mutta ajavat vielä pitkään asiansa. Yksi tuoreempi vuokralainen ja kaksi todella pitkäaikaista sopimusta!",
     images: [],
     status: 'vapaa',
   },
-  // {
-  //   id: 'A7',
-  //   name: 'Peruskuntoinen kaksio',
-  //   type: '2h+k+p',
-  //   price: 39890,
-  //   rent: 582.93,
-  //   fee: 225,
-  //   roi: 11.02,
-  //   coc: 29.72,
-  //   sale: false,
-  //   desc: "Peruskuntoinen kaksio ollut useamman vuoden vuokralla luotettavalla pariskunnalla!",
-  //   images: [],
-  //   status: 'vapaa',
-  // },
+  {
+    id: 'A7',
+    name: 'Peruskuntoinen kaksio',
+    type: '2h+k+p',
+    price: 39890,
+    rent: 582.93,
+    fee: 225,
+    roi: 10.77,
+    coc: 28.89,
+    sale: false,
+    desc: "Peruskuntoinen kaksio ollut useamman vuoden vuokralla luotettavalla pariskunnalla!",
+    images: [],
+    status: 'vapaa',
+  },
 ];
 
 export const apartments = populateImages(rawApartments);
